a2df0099658885bba2061536c0e8dd3e2544b129
kirill.gaiduk <Kirill1056@list.ru>
2022-05-27 20:55:49 +0700

    Ball destroy particles

--- Assets/_Project/Scripts/Gameplay/BallMover.cs
+++ Assets/_Project/Scripts/Gameplay/BallMover.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Bowling.Input;
 using UnityEngine;
 using UnityEngine.Pool;
@@ -9,6 +10,8 @@ namespace Bowling.Gameplay
     {
         [SerializeField] private float _speed = 1.0f;
         [SerializeField] private float _balloonSpeed = 1.0f;
+        [SerializeField] private ParticleSystem _particleSystem;
+        [SerializeField] private MeshRenderer _view;
 
         private Rigidbody _rigidbody;
         private Transform _target;
@@ -28,8 +31,23 @@ namespace Bowling.Gameplay
             transform.localScale = Vector3.one * 0.1f;
         }
 
+        private void OnEnable()
+        {
+            _view.gameObject.SetActive(true);
+            _particleSystem.gameObject.SetActive(false);
+        }
+
         public void Destroy()
         {
+            StartCoroutine(DestroyRoutine());
+        }
+
+        private IEnumerator DestroyRoutine()
+        {
+            _view.gameObject.SetActive(false);
+            _particleSystem.gameObject.SetActive(true);
+            _particleSystem.Play();
+            yield return new WaitForSeconds(_particleSystem.main.duration);
             _pool.Release(this);
         }
 
